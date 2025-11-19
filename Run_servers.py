feb72c9ba97a5a613682786969a3236f766060f6
clamar92 <cla.mar92@gmail.com>
2025-11-19 12:59:05 +0100

    final version

--- Run_servers.py
+++ Run_servers.py
@@ -1,15 +1,23 @@
 import subprocess
+import sys
 
 def main():
-    # Server CoAP: visibile
-    subprocess.Popen(["python", "__pycache__/Server_coap.cpython-39.pyc"])
+    python_exec = sys.executable
 
-    # Server HTTP: completamente silenziato
-    subprocess.Popen(
-        ["python", "__pycache__/Server_http.cpython-39.pyc"],
+    coap_proc = subprocess.Popen([python_exec, "__pycache__/Server_coap.cpython-311.pyc"])
+    http_proc = subprocess.Popen(
+        [python_exec, "__pycache__/Server_http.cpython-311.pyc"],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
+
+    try:
+        print("Servers running. Press CTRL+C to stop.")
+        coap_proc.wait()
+    except KeyboardInterrupt:
+        print("Stopping servers...")
+        coap_proc.terminate()
+        http_proc.terminate()
 
 if __name__ == "__main__":
     main()
